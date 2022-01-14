03615e3b8973753228ad7d5109522982ab26435a
beamchristian <beamchristian@yahoo.com>
2022-01-14 00:02:46 -0500

    footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2020 by Christian Beam</div>
+      <div className="container">&copy;2022 by Christian Beam</div>
     </footer>
   );
 };
